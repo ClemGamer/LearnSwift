56dfe559b40ddf37e8250635136e4594f55bed52
ClemGamer <tsaiyouchen@gmail.com>
2023-06-14 20:41:11 +0800

    [1]update: description for optional

--- 01_TheBasics/08_Optional/main.swift
+++ 01_TheBasics/08_Optional/main.swift
@@ -1,15 +1,15 @@
 // type followed by a question mark
 // the question mark indicates that the value it contains is optional, might be Int or nil
 let statusCode: Int? = 404
-print(statusCode as Any)
+print(statusCode)
 
 // define an optional variable without providing a default value, the variable is automatically set to nil
 var statusMessage: String?
-print(statusMessage as Any)
+print(statusMessage)
 
 // text might be converted failed when text is "Hello", so the conversion will be Optional
 let text = "123"
 let convertedText = Int(text)
 if convertedText != nil {
-    print(convertedText as Any) // cast to 'Any' to silence warning
+    print(convertedText!) // use "!" when you ensure the optional contains a value
 }
