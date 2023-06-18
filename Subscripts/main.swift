09b9015cb7ddd8273745f766a2c1c7c1b027cae5
ClemGamer <tsaiyouchen@gmail.com>
2023-06-18 21:48:52 +0800

    [0]feat: Subscripts

---
+++ Subscripts/main.swift
@@ -0,0 +1,45 @@
+// Subscripts
+// 搜尋
+
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(repeating: 0.0, count: rows * columns)
+    }
+    func indexIsValid(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    // 定義 self[row,column] 的 getter 與 setter
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
+
+var matrix = Matrix(rows: 2, columns: 2)
+matrix[0, 1] = 1.5
+matrix[1, 0] = 3.2
+/*
+[[0.0, 1.5],
+ [3.2, 0.0]]
+*/
+
+// static
+enum Planet: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+    static subscript(n: Int) -> Planet {
+        return Planet(rawValue: n)!
+    }
+}
+let mars = Planet[4] // static subscript
+print(mars)
+// mars
